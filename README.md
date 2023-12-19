7ecb3d768e58d9b7b5945768fa8d0c63638102d9
SaltyhorzE <prakashvs.official@gmail.com>
2023-12-19 15:45:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Day-16-CALL-BACK-HELL-TASK
---
+++ script.js
@@ -0,0 +1,39 @@
+setTimeout(() => {
+    document.body.innerHTML = "10"
+    setTimeout(() => {
+        document.body.innerHTML = "9";
+        setTimeout(() => {
+            document.body.innerHTML = "8";
+            setTimeout(() => {
+                document.body.innerHTML = "7";
+                setTimeout(() => {
+                    document.body.innerHTML = "6";
+                    setTimeout(() => {
+                        document.body.innerHTML = "5";
+                        setTimeout(() => {
+                            document.body.innerHTML = "4";
+                            setTimeout(() => {
+                                document.body.innerHTML = "3";
+                                setTimeout(() => {
+                                    document.body.innerHTML = "2";
+                                    setTimeout(() => {
+                                        document.body.innerHTML = "1";
+                                        setTimeout(() => {
+                                            document.body.innerHTML = `
+                                            
+                                            <h3 style ="
+                                            text-shadow: 0 0 5px #FFF, 0 0 10px #FFF, 0 0 15px #FFF, 0 0 20px #49ff18, 0 0 30px #49FF18, 0 0 40px #49FF18, 0 0 55px #49FF18, 0 0 75px #49ff18; 
+                                            ">
+                                            Happy Independence Day!
+                                            </h3>`;
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
